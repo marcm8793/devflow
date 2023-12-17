7e8f98a2fd515825989634f9537c3a963b9ca0b4
marcm8793 <marcm9387@gmail.com>
2023-12-17 16:40:35 +0100

    Add new components and functionality

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,123 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+const questions = [
+  {
+    _id: "1",
+    title:
+      "Issou ayaaaaoooooooooffffffffffffffffffffffddddddddddddddddddddddddddddddd",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: {
+      _id: "1",
+      name: "Chankla",
+      picture: "/assets/images/avatars/1.jpg",
+      clerkId: "ckqzrjw5w0000g5t5r3j1p5bq",
+    },
+    upvotes: "10",
+    views: 100,
+    answers: [],
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title:
+      "center a div en dépit des mesures sanitriales issou gpt en sueur devant tant de dépit text nimp t'es mal",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: {
+      _id: "1",
+      name: "Chankla",
+      picture: "/assets/images/avatars/1.jpg",
+      clerkId: "ckqzrjw5w0000g5t5r3j1p5bq",
+    },
+    upvotes: "10",
+    views: 100,
+    answers: [],
+    createdAt: new Date(),
+  },
+  {
+    _id: "1",
+    title: "ayaaaoooo",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: {
+      _id: "1",
+      name: "Chankla",
+      picture: "/assets/images/avatars/1.jpg",
+      clerkId: "ckqzrjw5w0000g5t5r3j1p5bq",
+    },
+    upvotes: "10",
+    views: 100,
+    answers: [],
+    createdAt: new Date(),
+  },
+];
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href={"/ask-question"} className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+      <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
+    </>
+  );
 }
