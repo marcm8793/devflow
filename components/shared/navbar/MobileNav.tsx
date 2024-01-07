f27459ad7a876a0d93abb1375e403a43c619d0ef
marcm8793 <marcm9387@gmail.com>
2024-01-07 23:06:21 +0100

    Update site logo in layout and navbar components

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -71,9 +71,9 @@ const MobileNav = () => {
       >
         <Link href="/" className="flex items-center gap-1">
           <Image
-            src="/assets/images/site-logo.svg"
-            width={23}
-            height={23}
+            src="/assets/icons/icon3.svg"
+            width={35}
+            height={35}
             alt="DevFlow"
           />
 
